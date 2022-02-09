7f920c97df2ee3d309545df3be5da4aff643cc0e
iamsahu <praffulsahu@gmail.com>
2022-02-10 00:07:44 +0530

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,76 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity 0.8.6;
 
-contract Contract {}
+contract Contract {
+    uint256 bal = 200;
+    uint256 reward_remaining = 100;
+
+    function returnWithout() external returns (bool hello) {
+        hello = true;
+    }
+
+    function returnWith() external returns (bool hello) {
+        hello = true;
+        return hello;
+    }
+
+    function a() external {
+        uint256 d_reward = bal - reward_remaining;
+        d_reward = d_reward + 100;
+    }
+
+    function b() external {
+        uint256 d_reward = bal - reward_remaining + 100;
+    }
+
+    uint256[] array = [100];
+    uint256[] balance = [10];
+    uint256[] res = [0];
+    uint256 reward;
+    uint256 reward2;
+
+    function extraCalculation106() external {
+        uint256 receiveable = array[0] +
+            ((balance[0] * (uint256(10) - 10)) / 1e20);
+    }
+
+    function checkedDirective105() external {
+        uint256 startIndex = 10 - 1;
+    }
+
+    function uncheckedDirective105() external {
+        unchecked {
+            uint256 startIndex = 10 - 1;
+        }
+    }
+
+    function checkedArrayDirective105() external {
+        res[0] = array[0] - balance[0];
+    }
+
+    function uncheckedArrayDirective105() external {
+        unchecked {
+            res[0] = array[0] - balance[0];
+        }
+    }
+
+    function unnArit101() external {
+        reward += 1;
+    }
+
+    function noArit101() external {
+        reward2 = 1;
+    }
+
+    function forPre()external{
+        for(uint256 i = 0; i < 10; i++){
+            
+        }
+    }
+
+    function forNoPre() external{
+        for(uint256 i; i < 10; ++i){
+            
+        }
+    }
+}
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity 0.8.6;
 
 import "ds-test/test.sol";
+import "../Contract.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Contract con;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        con = new Contract();
+    }
+
+    function testReturnWithout() public {
+        con.returnWithout();
+    }
+
+    function testReturnWith() public {
+        con.returnWith();
+    }
+
+    function testa() public {
+        con.a();
+    }
+
+    function testb() public {
+        con.b();
+    }
+
+    function testextraCalc() public {
+        con.extraCalculation106();
+    }
+
+    function testcheckedDirective105() public {
+        con.checkedDirective105();
+    }
+
+    function testuncheckedDirective105() public {
+        con.uncheckedDirective105();
+    }
+
+    function testcheckedArrayDirective105() public {
+        con.checkedArrayDirective105();
+    }
+
+    function testuncheckedArrayDirective105() public {
+        con.uncheckedArrayDirective105();
+    }
+
+    function testunnArit101() public {
+        con.unnArit101();
+    }
+
+    function testnoArit101() public {
+        con.noArit101();
+    }
+
+    function testForPre() public{
+        con.forPre();
+    }
+
+    function testForNoPre() public{
+        con.forNoPre();
     }
 }
